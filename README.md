c2e4b66150fe44370fd79d88324fe96dcdd7b7d9
harshtiwarii <harshtiwarii002@gmail.com>
2021-07-08 13:33:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ngform-wzznfu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ngform-wzznfu)
---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>hello#</h1><br/>
+            <sub></sub>`
+ 
+})
+export class AppComponent  {
+ 
+}
